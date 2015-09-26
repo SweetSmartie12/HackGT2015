b1f75743aea4803239b26f5bdd6508faf8146704
Antonio Calabrese <antoniobrese@aol.com>
2015-09-25 23:28:19 -0400

    Added screens to the game.

--- core/src/com/gt/hackgt/HackGT.java
+++ core/src/com/gt/hackgt/HackGT.java
@@ -1,27 +1,52 @@
 package com.gt.hackgt;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.gt.hackgt.mainmenuscreen.MainMenuScreen;
+import com.gt.hackgt.splashscreen.SplashScreen;
 
-public class HackGT extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class HackGT extends Game {
+    public final static int CAMERA_WIDTH = 640;
+    public final static int CAMERA_HEIGHT = 480;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    public final static int SPLASH_SCREEN_NUMBER = 1;
+    public final static int MAIN_MENU_SCREEN_NUMBER = 2;
+
+    public SpriteBatch batch;
+    public OrthographicCamera camera;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, CAMERA_WIDTH, CAMERA_HEIGHT);
+        this.setScreen(new SplashScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    /**
+     * Used to switch between the various screens.
+     * @param screen the screen number to switch to.
+     */
+    public void switchScreen(int screen) {
+        switch (screen) {
+            case HackGT.SPLASH_SCREEN_NUMBER:
+                setScreen(new SplashScreen(this));
+                break;
+
+            case HackGT.MAIN_MENU_SCREEN_NUMBER:
+                setScreen(new MainMenuScreen(this));
+                break;
+        }
+    }
 }

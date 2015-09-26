fbfd7f74e0615c1aec97dd2c382a56948c9daf86
Antonio Calabrese <antoniobrese@aol.com>
2015-09-25 22:15:33 -0400

    Initial commit.

---
+++ desktop/src/com/gt/hackgt/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gt.hackgt.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gt.hackgt.HackGT;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HackGT(), config);
+	}
+}
